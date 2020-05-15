948225f46312e92f3343584e7b7df6214f734e13
Daniel Angulo <dangulo4@gmail.com>
2020-05-14 22:38:51 -0700

    initial commit of heroku deployment

---
+++ client/build/precache-manifest.59845640dd3a99407f37dfbd2261b588.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c348aa34e21a930f84e",
+    "url": "/static/css/main.9f0ef59b.chunk.css"
+  },
+  {
+    "revision": "4c348aa34e21a930f84e",
+    "url": "/static/js/main.ce507e2e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c7df09a61a2c37d2f3a9",
+    "url": "/static/js/2.5c25870c.chunk.js"
+  },
+  {
+    "revision": "f4e24f5c200154b1e18649edd50a72a0",
+    "url": "/index.html"
+  }
+];
